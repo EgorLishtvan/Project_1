84c7f6056cefe28c070982911dada3ea6fe7a485
Egor Lishtvan <Lishtvan>
2021-09-22 20:16:04 +0300

    Bool Shet

--- app/src/main/java/com/example/project_tms/Discount.kt
+++ app/src/main/java/com/example/project_tms/Discount.kt
@@ -1,11 +1,12 @@
 package com.example.project_tms
 
+import java.lang.ArithmeticException
+
 class Discount {
     var title: String = ""
         get() = field.uppercase()
 
     var descriptin: String? = null
-    var amount = "df"
         set(value) {
             field = if (value != null) {
                 if(value.length<=100){
@@ -13,24 +14,26 @@ class Discount {
                 }else{
                     value.substring(0,100)
                 }
-        }else{
-            value
+            }else{
+                value
+            }
+        }
+
+    var amount:Int? =null
+        set(value){
+            var field = try {
+                if (value != null) {
+                    1/value
+                } else {
+                    0
+                }
+
+            } catch (exception: ArithmeticException) {
+                0
             }
         }
     var url: String?=""
 }
-set(value){
-    var field = try {
-        var value = null
-        if (value != null) {
-1/value
-        }else{
-            0
-        }
-
-    }  catch(exception:AritimeException){
-        0
-    }
 
 fun main(){
   val Discount=Discount()
